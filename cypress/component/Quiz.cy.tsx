f83054b2ef7643e63cec55fbe6fc933aaa15abb7
Keith <34338831+keithrsialana@users.noreply.github.com>
2024-11-27 17:29:50 -0500

    completed component tests

---
+++ cypress/component/Quiz.cy.tsx
@@ -0,0 +1,52 @@
+import { mount } from 'cypress/react';
+import Quiz from '../../client/src/components/Quiz.js';
+import React from 'react';
+
+describe("Tech Quiz", () => {
+  const waitTime = 250;
+
+	beforeEach(() => {
+		mount(<Quiz />);
+	});
+
+	it("starts the quiz", () => {
+		cy.contains("Start Quiz").click();
+		cy.get(".card").should("exist");
+	});
+
+	it("shows the next question after answering", () => {
+		cy.contains("Start Quiz").click();
+		cy.get(".btn-primary").first().click();
+		cy.get(".card h2").should("exist");
+	});
+
+	it("ends the quiz after all questions are answered", () => {
+		cy.contains("Start Quiz").click();
+    for (let i = 0; i < 10; i++) {
+      cy.get(".btn-primary").contains("1").click();
+      cy.wait(waitTime)
+    }
+		cy.contains("Quiz Completed").should("be.visible");
+	});
+
+	it("displays the score after completion", () => {
+		cy.contains("Start Quiz").click();
+    for (let i = 0; i < 10; i++) {
+      cy.get(".btn-primary").contains("1").click();
+      cy.wait(waitTime)
+    }
+		cy.contains("Quiz Completed").should("be.visible");
+		cy.get(".alert.alert-success").should("contain", "Your score:");
+	});
+
+	it("allows starting a new quiz", () => {
+		cy.contains("Start Quiz").click();
+    for (let i = 0; i < 10; i++) {
+      cy.get(".btn-primary").contains("1").click();
+      cy.wait(waitTime)
+    }
+		cy.contains("Quiz Completed").should("be.visible");
+		cy.contains("Take New Quiz").click();
+		cy.get(".btn-primary").contains("1").should("be.visible");
+	});
+});
